e83b2f49da9e997dc857656d2e7f8a3532623c61
Marcio Woitek <woitek@usp.br>
2020-09-29 07:46:31 -0300

    Consertei pequenos erros.

--- installation_scripts/vim.sh
+++ installation_scripts/vim.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt-get install gvim -y
+sudo apt-get install vim-gtk3 -y
 sudo apt-get install neovim -y
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
--- installation_scripts/zsh.sh
+++ installation_scripts/zsh.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt-get install zsh -y
-sudo apt-get install curl -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sudo apt-get install autojump -y
