bbe30e2299d9650a256fb37c774f0a7ce4adbc08
Marcio Woitek <woitek@usp.br>
2020-09-29 07:22:37 -0300

    Acrescentei comandos para instalar alguns plugins úteis do ZSH.

--- installation_scripts/zsh.sh
+++ installation_scripts/zsh.sh
@@ -2,3 +2,6 @@
 sudo apt-get install zsh -y
 sudo apt-get install curl -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sudo apt-get install autojump -y
+sudo apt-get install zsh-syntax-highlighting -y
