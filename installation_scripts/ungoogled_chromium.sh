6ff435b41a3c260c2c6fd037e693337d6268b32c
Marcio Woitek <woitek@usp.br>
2020-09-29 07:49:41 -0300

    Adicionei um script para instalar o Ungoogled Chromium.

---
+++ installation_scripts/ungoogled_chromium.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo 'deb http://download.opensuse.org/repositories/home:/ungoogled_chromium/Debian_Buster/ /' | sudo tee /etc/apt/sources.list.d/home-ungoogled_chromium.list > /dev/null
+curl -s 'https://download.opensuse.org/repositories/home:/ungoogled_chromium/Debian_Buster/Release.key' | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home-ungoogled_chromium.gpg > /dev/null
+sudo apt-get update
+sudo apt-get install ungoogled-chromium -y
+sudo apt-get install -f
