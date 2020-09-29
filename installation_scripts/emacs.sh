bb504334889dea04fbd0375e91c5421644b74f4b
Marcio Woitek <woitek@usp.br>
2020-09-29 07:20:39 -0300

    Scripts de instalação dos meus editores de texto preferidos.

---
+++ installation_scripts/emacs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt-get install emacs -y
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
---
+++ installation_scripts/vim.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt-get install gvim -y
+sudo apt-get install neovim -y
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
