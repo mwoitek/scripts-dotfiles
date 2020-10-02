aec345bc50a2bb19c6e13e887bfb03f7b3ee0e9d
Marcio Woitek <woitek@usp.br>
2020-10-02 07:40:08 -0300

    Added a command to install the MS fonts. Refactored an if-statement.

--- installation_scripts/fonts.sh
+++ installation_scripts/fonts.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 sudo apt-get install fonts-powerline -y
+# The 'contrib' component of the Debian repositories has to be enabled for the following command to work.
+# It may be necessary to edit the file '/etc/apt/sources.list'.
+sudo apt-get install ttf-mscorefonts-installer -y
 cd
-if [ ! -d .fonts ]; then
-    mkdir .fonts
-fi
+[[ ! -d .fonts ]] && mkdir .fonts
 cd .fonts
 git clone --depth 1 https://github.com/ryanoasis/nerd-fonts
 fc-cache -fv
