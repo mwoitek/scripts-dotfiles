590548c8858f3010e69b8cc112c37bdeda7d62c6
Marcio Woitek <woitek@usp.br>
2020-10-12 20:56:51 -0300

    Alterei o meu script de atualização.
    Especificamente, fiz o seguinte:
    Refatorei o código que utiliza o git;
    Adicionei um comando para atualizar os plugins do neovim.

--- atual.sh
+++ atual.sh
@@ -20,13 +20,28 @@
 #
 #
 # Script que atualiza os programas nos meus computadores que rodam o Debian.
+
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get autoremove -y
 sudo apt-get clean
-cd ~/.fonts/nerd-fonts
-git pull
-cd ~/.fzf
-git pull
-cd ~/trash-cli
-git pull
+
+function gitPull() {
+    echo ""
+    echo "Atualizando $1..."
+    if [[ -d "$2" ]]; then
+        cd "$2"
+        git pull
+    else
+        echo "O diretório $2 não existe."
+    fi
+}
+
+gitPull "fzf" "${HOME}/.fzf"
+# gitPull "neovim" "${HOME}/neovim"
+gitPull "nerd-fonts" "${HOME}/.fonts/nerd-fonts"
+gitPull "trash-cli" "${HOME}/trash-cli"
+
+echo ""
+echo "Atualizando os plugins do Neovim..."
+nvim -c 'PlugUpdate|PlugUpgrade|quitall'
